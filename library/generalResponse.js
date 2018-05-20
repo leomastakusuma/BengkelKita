fa30d9303d244851625a92fddd209e553f48824e
leomastakusuma <leomastakusumaa@gmail.com>
2018-05-20 13:16:54 +0700

    update

---
+++ library/generalResponse.js
@@ -0,0 +1,60 @@
+export class generalResponse {
+	constructor(){
+		this.result = ""
+	}
+	responseSuccess(message, data, callback) {
+		this.result = {
+			status: 200,
+			message: "success",
+			display_message: message,
+			data: data,
+		}
+		callback(this.result)
+	}
+	responseUpdate(callback) {
+		this.result = {
+			status: 200,
+			status_message: "update success",
+			display_message: "Success update your data",
+			data: {},
+		}
+		callback(this.result)
+	}
+
+	responseNotFound(callback) {
+		this.result = {
+			status: 204,
+			message: "not found",
+			display_message: "opps data not found",
+			data: {},
+		}
+		callback(this.result)
+	}
+	responseerrorAuth(data, callback) {
+		this.result = {
+			status: 401,
+			message: "error",
+			display_message: "Error Autentification",
+			data: {},
+		}
+		callback(this.result)
+	}
+	responseISExist(display_message, data, callback) {
+		var result = {
+			status: 409,
+			message: "error",
+			display_message: "field data is already exist",
+			data: {}
+		}
+		callback(result)
+	}
+	responseFailed(display_message, data, callback) {
+		var result = {
+			status: 500,
+			message: "error",
+			display_message: "Opp someting wrong,please try again",
+			data: data
+		}
+		callback(result)
+	}
+}
