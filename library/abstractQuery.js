6c0743cda88246ecf43c5e977b479e47cc9a8762
leomastakusuma <leomastakusumaa@gmail.com>
2018-05-31 10:46:08 +0700

    update

--- library/abstractQuery.js
+++ library/abstractQuery.js
@@ -1,6 +1,6 @@
 import mysql from "mysql"
 import config from "config"
-
+import abstractBengkelKita from "../library/abstractBengkelKita"
 export default class abstractQuery {
 	queryEscape(Query, Params, callback) {
 		let conf_core_sys = config.get("CoreSys")
@@ -19,14 +19,24 @@ export default class abstractQuery {
 				connection.release()
 				throw err
 			}
-			connection.query(Query, Params, function (err, results) {
+			let Log = connection.query(Query, Params, function (err, results) {
 				connection.release()
 				if (!err) {
+					new abstractBengkelKita().sendTelegram(Log.sql)
 					callback(results)
 				}
 			})
 			connection.on("error", function (err) {
 				throw err
+			})
+			connection.on("acquire", function (connection) {
+				new abstractBengkelKita().sendTelegram("Connection %d acquired", connection.threadId)
+			})
+			connection.on("enqueue", function () {
+				new abstractBengkelKita().sendTelegram("Waiting for available connection slot")
+			})
+			connection.on("release", function (connection) {
+				new abstractBengkelKita().sendTelegram("Connection %d released", connection.threadId)
 			})
 		})
 	}
