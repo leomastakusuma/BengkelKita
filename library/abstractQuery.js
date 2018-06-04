d56957189c429213a0e7cec1a85630ff8d0fc4cf
leomastakusuma <leomastakusumaa@gmail.com>
2018-06-04 12:56:29 +0700

    update

--- library/abstractQuery.js
+++ library/abstractQuery.js
@@ -1,6 +1,4 @@
-// import abstractBengkelKita from "../library/abstractBengkelKita"
 import dbConfig from "./../dbConfig"
-
 export default class abstractQuery {
 	queryEscape(Query, Params, callback) {
 		dbConfig.query(Query, Params, function (err, results) {
@@ -8,30 +6,6 @@ export default class abstractQuery {
 				callback(results)
 			}
 		})
-
-
-
-		// poolingQuery.getConnection(function (err, connection) {
-		// 	if (err) {
-		// 		console.log(err)
-		// 		throw err
-		// 	}
-		// 	connection.query(Query, Params, function (err, results) {
-		// 		connection.release()
-		// 		if (!err) {
-		// 			callback(results)
-		// 		}
-		// 	})
-		// 	connection.on("error", function (err) {
-		// 		new abstractBengkelKita().sendTelegram("Error Db"+err)
-		// 	})
-		// 	connection.on("enqueue", function () {
-		// 		new abstractBengkelKita().sendTelegram("Waiting for available connection slot")
-		// 	})
-		// 	connection.on("release", function (connection) {
-		// 		new abstractBengkelKita().sendTelegram("Connection released"+connection.threadId)
-		// 	})
-		// })
 	}
 }
 
